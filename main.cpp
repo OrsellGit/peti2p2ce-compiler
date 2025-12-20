353dda213728a9b1abef9feea3723c91b1732d34
Orsell <34631691+OrsellGit@users.noreply.github.com>
2025-12-20 01:19:24 -0700

    feat: This custom VBSP is now able to be loaded by P2, VBSP part itself however does not

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
+#include <filesystem>
 #include <iostream>
 
+/**
+ * @brief Portal 2 will run this version of VBSP instead of its own.
+ *        Here various actions will occur before the original VBSP EXE is called.
+ * @param argc Number of arguments passed to the program.
+ * @param argv Arguments passed to the program.
+ * @return Return code of the program. Portal 2 will recognize any return code that is non-zero to be a error code.
+ */
 int main( const int argc, char** argv )
 {
     for (int i = 0; i < argc; i++ )
         std::cout << argv[i] << std::endl;
 
-    std::cout << "TEST TEST TEST" << std::endl;
+    std::filesystem::path vbspPath = argv[0];
+    vbspPath.replace_filename("vbsp_original.exe");
 
-    if (true)
-        return -1;
-
-    return 0;
+    return execl(vbspPath.string().c_str(), argv[1], argv[2], argv[3], argv[4], argv[5], static_cast<char*>(nullptr));
 }
