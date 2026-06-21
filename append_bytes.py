5f41c0e0e7d0e1e4b621d08651d984401bf13a11
Orsell <34631691+OrsellGit@users.noreply.github.com>
2026-06-21 12:10:49 -0700

    fix: Added comment about what the post compile script does

--- append_bytes.py
+++ append_bytes.py
@@ -1,3 +1,5 @@
+# This script is needed for post-compile in order to append bytes to the EXEs that PyInstaller executables have in order for BEEMod not to replace these compilers.
+
 import pathlib, sys
 
 with pathlib.Path(sys.argv[1]).open("ab") as f:
